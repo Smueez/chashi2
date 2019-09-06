eab72c72687157dc76d9b57afe04baac9b27554a
akash <akashshahriar55@yahoo.com>
2019-09-06 21:16:33 +0600

    Chasi v1.1

---
+++ app/src/main/java/com/example/chashi/ui/home/HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.chashi.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+
+import com.example.chashi.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
+}
