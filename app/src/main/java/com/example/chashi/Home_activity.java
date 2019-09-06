82ed62567ac66c9e04d87a6a727f2f7f48d58643
Smueez <mueezsheersho95@gmail.com>
2019-09-06 19:41:21 +0600

    update5

---
+++ app/src/main/java/com/example/chashi/Home_activity.java
@@ -0,0 +1,15 @@
+
+package com.example.chashi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_activity);
+    }
+}
