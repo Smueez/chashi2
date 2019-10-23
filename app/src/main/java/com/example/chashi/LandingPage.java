209a05b85b6bcc1368508ece020ac0f700f687eb
me.tareqjoy@gmail.com <HEXAdecimal6875>
2019-10-23 22:58:36 +0600

    tareq1

--- app/src/main/java/com/example/chashi/LandingPage.java
+++ app/src/main/java/com/example/chashi/LandingPage.java
@@ -43,7 +43,7 @@ public class LandingPage extends AppCompatActivity  {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_detect, R.id.nav_profile, R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
