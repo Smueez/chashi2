3dcf4a360b855bfe7fd0748801c812f6613267db
me.tareqjoy@gmail.com <HEXAdecimal6875>
2019-12-05 00:37:13 +0600

    tareq7

--- app/src/main/java/com/example/chashi/CallChargeAPI.java
+++ app/src/main/java/com/example/chashi/CallChargeAPI.java
@@ -60,20 +60,25 @@ public class CallChargeAPI extends AsyncTask<String, String, DOBTransaction> {
         try {
             OkHttpClient client = new OkHttpClient();
 
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, "{ \"sourceId\": \"AGWKuetG\", \"idType\": \"MSISDN\",\"serviceId\": \"PPU00021805630\", \"transactionPin\": \"" + otp + "\", \"otpTransactionId\": \""+otpTranId+"\", \"category\": \"app\", \"description\": \"pay first\" }");
             Request request = new Request.Builder()
-                    .url("https://apigw.grameenphone.com:9001/payments/v2/customers/" + phnNo + "/chargeotp")
-                    .post(body)
-                    .addHeader("Accept-encoding", "application/gzip")
-                    .addHeader("Content-Type", "application/json")
-                    .addHeader("Authorization", "Bearer " + accessToken)
-                    .addHeader("User-Agent", "PostmanRuntime/7.18.0")
+                    .url("https://developer.bdapps.com/caas/direct/debit")
+                    .post(null)
+                    .addHeader("externalTrxId", "234444")
+                    .addHeader("amount", "5")
+                    .addHeader("applicationId", "APP_020950")
+                    .addHeader("password", "3a8821bf20e300e8cda37dcc9e68bcf5")
+                    .addHeader("subscriberId", "tel: 8801620601009")
+                    .addHeader("paymentInstrumentName", "Mobile Account")
+                    .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                    .addHeader("accountId", "8801620601009")
+                    .addHeader("currency", "BDT")
+                    .addHeader("User-Agent", "PostmanRuntime/7.20.1")
                     .addHeader("Accept", "*/*")
                     .addHeader("Cache-Control", "no-cache")
-                    .addHeader("Postman-Token", "9e5e85ea-225e-4842-88f7-5efebf547e4a,6e6009d9-f3a7-49ee-b8fb-a2e0d511c365")
-                    .addHeader("Host", "apigw.grameenphone.com:9001")
-                    .addHeader("Content-Length", "198")
+                    .addHeader("Postman-Token", "efadc0b0-caab-44d0-8461-fc32b4bbad3c,855e0da3-6929-4276-926b-75ca729ffe71")
+                    .addHeader("Host", "developer.bdapps.com")
+                    .addHeader("Accept-Encoding", "gzip, deflate")
+                    .addHeader("Content-Length", "0")
                     .addHeader("Connection", "keep-alive")
                     .addHeader("cache-control", "no-cache")
                     .build();
@@ -81,11 +86,11 @@ public class CallChargeAPI extends AsyncTask<String, String, DOBTransaction> {
             Response response = client.newCall(request).execute();
             String res = response.body().string();
             JSONObject userObject = new JSONObject(res);
-            if (userObject.has("code")) {
-                String code = userObject.getString("code");
-                String message = userObject.getString("message");
-                String transactionId = userObject.getString("transactionId");
-                String time = userObject.getJSONObject("accessInfo").getString("timestamp");
+            if (userObject.has("statusCode")) {
+                String code = userObject.getString("statusCode");
+                String message = userObject.getString("statusDetail");
+                String transactionId = userObject.getString("internalTrxId");
+                String time = userObject.getString("timeStamp");
                 return new DOBTransaction(true, code, message, transactionId, time);
             } else {
                 String time = userObject.getJSONObject("accessInfo").getString("timestamp");
